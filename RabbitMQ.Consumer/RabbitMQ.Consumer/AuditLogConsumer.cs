63b520a99f39b01d0251b33545c66c3f857bb306
Flyear <flyear@flyear.net>
2016-01-04 19:41:03 +0800

    submit consumer

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/AuditLogConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Rebus.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    public class AuditLogConsumer : IHandleMessages<TestMessage>
+    {
+        public System.Threading.Tasks.Task Handle(TestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TestMessage
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Castle.MicroKernel.Registration;
+using RabbitMQ.Client;
+using Rebus.CastleWindsor;
+using Rebus.Config;
+using Rebus.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rebus.NLog;
+using Rebus.RabbitMq;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverName = ConfigurationManager.AppSettings["RabbitServer"];
+            var queueName = ConfigurationManager.AppSettings["QueueName"];
+
+            var container = new Castle.Windsor.WindsorContainer();
+            var adapter = new CastleWindsorContainerAdapter(container);
+
+            container.Register(Classes.FromAssemblyContaining<AuditLogConsumer>()
+           .BasedOn<IHandleMessages>()
+           .WithServiceAllInterfaces()
+           .LifestyleTransient());
+
+            var bus = Configure.With(adapter)
+                .Options(i => i.SetNumberOfWorkers(10).SetMaxParallelism(1))
+                .Logging(i => i.NLog())
+                .Transport(t => t.UseRabbitMq(serverName, queueName))
+                .Start();
+
+            bus.Subscribe<TestMessage>();
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
