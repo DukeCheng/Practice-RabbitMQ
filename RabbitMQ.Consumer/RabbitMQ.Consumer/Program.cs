aab4b8768020b5ea4ed46da45cb2b90b9950f77a
Flyear <flyear@flyear.net>
2016-01-07 13:25:46 +0800

    use fanout type

--- RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -21,34 +21,32 @@ namespace RabbitMQ.Consumer
         static void Main(string[] args)
         {
            var serverName = ConfigurationManager.AppSettings["RabbitServer"];
-            var queueName = ConfigurationManager.AppSettings["QueueName"];
+            //var queueName = ConfigurationManager.AppSettings["QueueName"];
 
             var factory = new ConnectionFactory() { HostName = ConfigurationManager.AppSettings["RabbitServer"], UserName = "dev", Password = "dev", VirtualHost = "dev_host" };
 
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.ExchangeDeclare(exchange: "xa_test_logs", type: "fanout");
 
-                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "xa_test_logs",
+                                  routingKey: "");
 
-                Console.WriteLine(" [*] Waiting for messages.");
+                Console.WriteLine(" [*] Waiting for logs.");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-
-                    int dots = message.Split('.').Length - 1;
-                    Thread.Sleep(dots * 1000);
-
-                    Console.WriteLine(" [x] Done");
-
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    Console.WriteLine(" [x] {0}", message);
                 };
-                channel.BasicConsume(queue: "task_queue", noAck: false, consumer: consumer);
+                channel.BasicConsume(queue: queueName,
+                                     noAck: true,
+                                     consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
